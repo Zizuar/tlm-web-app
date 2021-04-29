df0e4700ce854d5c4b3d2f60366b0a82ce30eb5a
MikeSierra <reklam11@gmail.com>
2021-04-29 14:13:21 +0000

    Add webp compatibility (Modernizr); update content

--- public/js/2021.js
+++ public/js/2021.js
@@ -9,4 +9,15 @@ $(document).ready(() => {
         endtimeSeconds: 00,
         timeZone: "America/New_York" 
     });
+
+    $('.countdown-emergency').countdown100({
+        /*Set Endtime here*/
+        endtimeYear: 2021,
+        endtimeMonth: 05,
+        endtimeDate: 28,
+        endtimeHours: 0,
+        endtimeMinutes: 00,
+        endtimeSeconds: 00,
+        timeZone: "America/New_York" 
+    });
 });
