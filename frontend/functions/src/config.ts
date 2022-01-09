745cc50047da9c9689fd7d4f178922a4139ac9d3
Gabor Meszaros <reklam11@gmail.com>
2022-01-09 17:30:16 +0100

    Deployment fixes
    Fix action file extension
    Deployment fixes
    Remove NestJS remnants from functions folder

--- frontend/functions/src/config.ts
+++
@@ -1,4 +0,0 @@
-import * as functions from 'firebase-functions';
-
-export const ENVIRONMENT = functions.config()['environment'] || {};
-export const CONFIG = functions.config()['config'] || {};
--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   recaptcha: {
     sitekey: '6Ley8FwaAAAAAKrEXqlKLPkKivxIkfVN6VnnoHP7'
   },
-  apiBaseUrl: '',
+  apiBaseUrl: 'https://tlm-api.herokuapp.com',
 };
