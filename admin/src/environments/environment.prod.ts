e9f88dc5df777a8de4f4bef1e616757cf46aeabf
Gabor Meszaros <reklam11@gmail.com>
2022-11-22 22:12:19 +0100

    Fix build errors

--- admin/src/environments/environment.prod.ts
+++ admin/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ import { Environment } from '../app/core/models/environment';
 
 export const environment: Environment = {
   production: true,
-  baseUrl: 'https://tylerlevs.com',
+  baseUrl: 'https://admin.tylerlevs.com',
   apiBaseUrl: 'https://api.tylerlevs.com',
   auth: {
     clientId: 'QI6OizFa5JpBocC82KG2DXSX9VkSev9Y',
